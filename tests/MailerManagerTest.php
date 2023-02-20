b4c5c0caf7e51b85cbc28203f30216065cf04a56
Gauthier Boddaert <boddaert.gauthier@gmail.com>
2023-02-20 15:02:16 +0100

    start phpUnitTest

---
+++ tests/MailerManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Service;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MailerManagerTest extends KernelTestCase
+{
+    public function testSomething()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
